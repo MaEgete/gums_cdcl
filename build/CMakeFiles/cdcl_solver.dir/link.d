a862202b5e9faf616b236aec597b259bd614e230
MaEgete <MaEgetemeyer@t-online.de>
2026-01-30 15:49:58 +0100

    test

---
+++ build/CMakeFiles/cdcl_solver.dir/link.d
@@ -0,0 +1,112 @@
+cdcl_solver: \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crt1.o \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crti.o \
+  /usr/lib/gcc/x86_64-redhat-linux/15/crtbegin.o \
+  CMakeFiles/cdcl_solver.dir/main.cpp.o \
+  CMakeFiles/cdcl_solver.dir/Clause.cpp.o \
+  CMakeFiles/cdcl_solver.dir/Solver.cpp.o \
+  CMakeFiles/cdcl_solver.dir/Trail.cpp.o \
+  CMakeFiles/cdcl_solver.dir/CNFParser.cpp.o \
+  CMakeFiles/cdcl_solver.dir/Heuristic.cpp.o \
+  CMakeFiles/cdcl_solver.dir/Literal.cpp.o \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libstdc++.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so \
+  /lib64/libm.so.6 \
+  /lib64/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /lib64/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so \
+  /lib64/libc.so.6 \
+  /usr/lib64/libc_nonshared.a \
+  /lib64/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /lib64/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/15/crtend.o \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crtn.o \
+  /lib64/ld-linux-x86-64.so.2
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crt1.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crti.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/crtbegin.o:
+
+CMakeFiles/cdcl_solver.dir/main.cpp.o:
+
+CMakeFiles/cdcl_solver.dir/Clause.cpp.o:
+
+CMakeFiles/cdcl_solver.dir/Solver.cpp.o:
+
+CMakeFiles/cdcl_solver.dir/Trail.cpp.o:
+
+CMakeFiles/cdcl_solver.dir/CNFParser.cpp.o:
+
+CMakeFiles/cdcl_solver.dir/Heuristic.cpp.o:
+
+CMakeFiles/cdcl_solver.dir/Literal.cpp.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libstdc++.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so:
+
+/lib64/libm.so.6:
+
+/lib64/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/lib64/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so:
+
+/lib64/libc.so.6:
+
+/usr/lib64/libc_nonshared.a:
+
+/lib64/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/lib64/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/crtend.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crtn.o:
+
+/lib64/ld-linux-x86-64.so.2:
